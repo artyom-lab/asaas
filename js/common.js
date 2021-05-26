ecd38e1112dee49f1e9dd95eedd5ca8772723fb4
bodyanba <galkinbogdan@gmail.com>
2021-05-26 19:04:43 +0300

    uploaders

--- js/common.js
+++ js/common.js
@@ -283,168 +283,26 @@ $(function() {
     delayPopup(this);
   });
 
-  $(document).on('change', '.up', function() {
-    var profilePicValue = $(this).val();
-    var fileNameStart = profilePicValue.lastIndexOf('\\'); /* finds the end of the filepath */
+  var placeholder;
+  $(document).on('change', '.uploader-input', function() {
+    var input = $(this),
+        profilePicValue = input.val(),
+        fileNameStart = profilePicValue.lastIndexOf('\\'); /* finds the end of the filepath */
     profilePicValue = profilePicValue.substr(fileNameStart + 1).substring(0, 20); /* isolates the filename */
-    //var profilePicLabelText = $(".upl"); /* finds the label text */
+    placeholder = input.siblings('.form-control').find('.image').attr('src');
     if (profilePicValue != '') {
-      //console.log($(this).closest('.fileUpload').find('.upl').length);
-      $(this).siblings('.fileUpload').find('.upl').html(profilePicValue); /* changes the label text */
-      $(this).siblings('.delete-small').show();
+      input.siblings('.form-control').children('.uploader-text').html(profilePicValue);
+      input.closest('.uploader').addClass('uploaded');
     };
-  });
-  $(".delete-small").click(function() {
-    $(this).hide().siblings(".up").val("").siblings(".fileUpload").find('.upl').html("Upload your auction license certificate");
-  });
-
-  function ekUpload() {
-    function Init() {
-
-      console.log("Upload Initialised");
-
-      var fileSelect    = document.getElementById('file-upload'),
-          fileDrag      = document.getElementById('file-drag');
-
-      fileSelect.addEventListener('change', fileSelectHandler, false);
-
-      // Is XHR2 available?
-      var xhr = new XMLHttpRequest();
-      if (xhr.upload) {
-        // File Drop
-        fileDrag.addEventListener('dragover', fileDragHover, false);
-        fileDrag.addEventListener('dragleave', fileDragHover, false);
-        fileDrag.addEventListener('drop', fileSelectHandler, false);
-      }
-    }
-
-    function fileDragHover(e) {
-      var fileDrag = document.getElementById('file-drag');
-
-      e.stopPropagation();
-      e.preventDefault();
-
-      fileDrag.className = (e.type === 'dragover' ? 'hover' : 'modal-body file-upload');
-    }
-
-    function fileSelectHandler(e) {
-      // Fetch FileList object
-      var files = e.target.files || e.dataTransfer.files;
-
-      // Cancel event and hover styling
-      fileDragHover(e);
-
-      // Process all File objects
-      for (var i = 0, f; f = files[i]; i++) {
-        parseFile(f);
-        uploadFile(f);
-      }
-    }
-
-    // Output
-    function output(msg) {
-      // Response
-      var m = document.getElementById('messages');
-      m.innerHTML = msg;
-    }
-
-    function parseFile(file) {
-
-      console.log(file.name);
-      output(
-        '<strong>' + encodeURI(file.name) + '</strong>'
-      );
-      
-      // var fileType = file.type;
-      // console.log(fileType);
-      var imageName = file.name;
-
-      var isGood = (/\.(?=gif|jpg|png|jpeg)/gi).test(imageName);
-      if (isGood) {
-        document.getElementById('start').classList.add("hidden");
-        document.getElementById('notimage').classList.add("hidden");
-        document.getElementById('response').classList.remove("hidden");
-        // Thumbnail Preview
-        document.getElementById('file-image').classList.remove("hidden");
-        document.getElementById('file-image').src = URL.createObjectURL(file);
-      }
-      else {
-        document.getElementById('start').classList.remove("hidden");
-        document.getElementById('notimage').classList.remove("hidden");
-        document.getElementById('response').classList.add("hidden");
-        document.getElementById('file-image').classList.add("hidden");
-        document.getElementById("file-upload-form").reset();
-      }
-    }
-
-    function setProgressMaxValue(e) {
-      var pBar = document.getElementById('file-progress');
-
-      if (e.lengthComputable) {
-        pBar.max = e.total;
-      }
-    }
-
-    function updateFileProgress(e) {
-      var pBar = document.getElementById('file-progress');
-
-      if (e.lengthComputable) {
-        pBar.value = e.loaded;
-      }
-    }
-
-    function uploadFile(file) {
-      
-      var xhr = new XMLHttpRequest(),
-        fileInput = document.getElementById('class-roster-file'),
-        pBar = document.getElementById('file-progress'),
-        fileSizeLimit = 1024; // In MB
-      if (xhr.upload) {
-        // Check if file is less than x MB
-        if (file.size <= fileSizeLimit * 1024 * 1024) {
-          // Progress bar
-          pBar.style.display = 'inline';
-          xhr.upload.addEventListener('loadstart', setProgressMaxValue, false);
-          xhr.upload.addEventListener('progress', updateFileProgress, false);
-
-          // File received / failed
-          xhr.onreadystatechange = function(e) {
-            if (xhr.readyState == 4) {
-              // Everything is good!
-              // progress.className = (xhr.status == 200 ? "success" : "failure");
-              // document.location.reload(true);
-            }
-          };
-          // Start upload
-          xhr.open('POST', document.getElementById('file-upload-form').action, true);
-          xhr.setRequestHeader('X-File-Name', file.name);
-          xhr.setRequestHeader('X-File-Size', file.size);
-          xhr.setRequestHeader('Content-Type', 'multipart/form-data');
-          xhr.send(file);
-        } else {
-          output('Please upload a smaller file (< ' + fileSizeLimit + ' MB).');
-        }
-      }
-    }
-    // Check for the various File API support.
-    if (window.File && window.FileList && window.FileReader) {
-      Init();
-    } else {
-      document.getElementById('file-drag').style.display = 'none';
-    }
-  }
-  ekUpload();
-
-  $(".delete-img").click(function() {
-    document.getElementById('start').classList.remove("hidden");
-    document.getElementById('notimage').classList.add("hidden");
-    document.getElementById('response').classList.add("hidden");
-    document.getElementById('file-image').classList.add("hidden");
-    document.getElementById('file-image').classList.add("hidden");
-    document.getElementById('file-image').src = "#!";
-    $("#file-drag").removeClass("modal-body").removeClass("file-upload");
-    $("#file-upload").val("");
-    // $(this).hide().siblings(".up").val("").siblings(".fileUpload").find('.upl').html("Upload your auction license certificate");
+    let file = this.files[0]; 
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      input.siblings('.form-control').find('.image').attr('src', e.target.result);
+    };
+    reader.readAsDataURL(file);
+  }).on('click', '.delete', function() {
+    $(this).closest('.uploader').removeClass('uploaded').find('input[type="file"]').val('').siblings('.form-control').children('.uploader-text').html('Upload your auction license certificate');
+    $(this).closest('.uploader').find('.image').attr('src', placeholder);
   });
 
 });
